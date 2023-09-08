8bb162166439647dbea1aa1c8c6d7af9ca0e8c28
mobina naseri <mobinanaseri11@gmail.com>
2023-09-08 13:03:44 +0330

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <VitePwaManifest />
+  <LayoutHeader />
+  <main class="container my-5 max-w-4xl">
+    <NuxtPage />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <SectionsAboutMe />
+  <SectionsSkillIcons />
+  <SectionsProjectsList />
+</template>
